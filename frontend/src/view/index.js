e4fffd6f2e514aa8ba69fdbc755ede7cc3e363b2
Marcos <marcospaulorgomes.dev@gmail.com>
2021-02-04 04:13:14 -0300

    View: component name e index contet change

---
+++ frontend/src/view/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import commentInterface from './components/commentInterface'
+
+const commentInterfaceCont = () => {
+    return <commentInterface/>
+}
+
+export default commentInterfaceCont;
